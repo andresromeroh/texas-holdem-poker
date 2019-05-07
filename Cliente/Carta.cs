b7141ef01a4012fd9a9400eec1eed574c1c90cb8
Andrés Romero <andres.romero22@outlook.com>
2019-05-06 20:19:46 -0600

    Clase Carta

---
+++ Cliente/Carta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    class Carta
+    {
+        string Leyenda {
+            get { return Leyenda; }
+            set { Leyenda = value; }
+        }
+
+        int TipoPalo
+        {
+            get { return TipoPalo; }
+            set { TipoPalo = value; }
+        }
+
+        public static int ESPADAS = 1;
+        public static int CORAZONES = 2;
+        public static int DIAMANTES = 3;
+        public static int TREBOLES = 14;
+
+
+        // Constructor que no toma argumentos:
+        public Carta()
+        {
+            Leyenda = "";
+            TipoPalo = 0;
+        }
+
+        // Constructor que toma argumentos:
+        public Carta(string leyanda, int tipoPalo)
+        {
+            Leyenda = leyanda;
+            TipoPalo = tipoPalo;
+        }
+
+    }
+}
